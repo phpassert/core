99bdc4a3a6648ebec964df4621cf3e5faa99921d
Thomas Farla <thomas-farla@hotmail.com>
2015-11-08 14:56:08 +0100

    Remove execution info

---
+++ src/PHPAssert/Core/Result/CompoundResult.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 08/11/15
+ * Time: 14:55
+ */
+
+namespace PHPAssert\Core\Result;
+
+
+class CompoundResult
+{
+
+}
---
+++ tests/PHPAssert/Core/Result/CompoundResultTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thomas
+ * Date: 08/11/15
+ * Time: 14:55
+ */
+
+namespace unit\PHPAssert\Core\Result;
+
+
+class CompoundResultTest extends \PHPUnit_Framework_TestCase
+{
+
+}
